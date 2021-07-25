0b843bf44b6208acf07c72ab56d20279543f0362
Mohammad Elnaggar <moelnaggar14@gmail.com>
2021-07-25 18:01:36 +0200

    add assets

---
+++ iDine/Scenes/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  iDine
+//
+//  Created by Mohammed Elnaggar on 25/07/2021.
+//  Copyright © 2021 Mohammed Elnaggar. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
