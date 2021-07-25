48d438cc709a516bc8377d1d84636ad8176423f3
Mohammad Elnaggar <moelnaggar14@gmail.com>
2021-07-25 17:48:15 +0200

    add initial project

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mohammed Elnaggar on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
