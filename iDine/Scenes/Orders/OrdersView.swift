1fa6c05506d5bba30695d2206e03a8aac2beebe7
Mohammad Elnaggar <moelnaggar14@gmail.com>
2021-07-26 01:36:49 +0200

    add OrdersView

---
+++ iDine/Scenes/Orders/OrdersView.swift
@@ -0,0 +1,45 @@
+//
+//  OrdersView.swift
+//  iDine
+//
+//  Created by Mohammed Elnaggar on 26/07/2021.
+//  Copyright © 2021 Mohammed Elnaggar. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrdersView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$0\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Checkout")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationTitle("Orders")
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+}
+
+struct OrdersView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrdersView()
+            .environmentObject(Order())
+    }
+}
